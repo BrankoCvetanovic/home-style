37e7cb20305838278e4492039a955c989fd89a59
BahkoHatake <cbranko01@gmail.com>
2025-01-18 16:12:01 +0100

    Add ImageSlider, Add Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="logo">Logo</div>
+      <div className="nav-bar">
+        <div>Home</div>
+        <div>Galery</div>
+        <div>Contact</div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/data/sliderImages.js
@@ -0,0 +1,13 @@
+import image1 from "../assets/csm_glavna-Izbor-boje-za-atraktivne-ambijente-doma_464a2734a6.jpg";
+import image2 from "../assets/images.jpg";
+import image3 from "../assets/Kuhinja-Atina-2-e1730295717987.png";
+import image4 from "../assets/premium_photo-1680382578857-c331ead9ed51.jpg";
+
+const sliderImages = [
+  { imgURL: image1, imgAlt: "img1" },
+  { imgURL: image2, imgAlt: "img2" },
+  { imgURL: image3, imgAlt: "img3" },
+  { imgURL: image4, imgAlt: "img4" },
+];
+
+export default sliderImages;
