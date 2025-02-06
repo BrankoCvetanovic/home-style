b2ff2dc7b15cac3fc1911e32811aea868fe039aa
BahkoHatake <cbranko01@gmail.com>
2025-02-06 20:52:17 +0100

    Add contact page

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({ imgURL, imgAlt, note }) {
   return (
     <>
-      <img src={imgURL} alt={imgAlt} />
+      <img src={imgURL} alt={imgAlt} loading="lazy" />
       <div className="image-note">
         {note.first}
         <span>{note.second}</span>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,33 @@
+import { Email, Phone } from "@mui/icons-material";
+import kuhinja from "../assets/Kuhinja-Atina-2-e1730295717987.png";
+const apiKey = import.meta.env.VITE_API_KEY;
+
 export default function ContactPage() {
   return (
-    <div className="contact">
-      <h1>Contact Page</h1>
+    <div className="contact-page">
+      <div className="image-container">
+        <img src={kuhinja} alt="Contact Illustration" />
+      </div>
+      <div className="contact-container">
+        <h1>Kontakt</h1>
+        <div className="contact-info">
+          <p>
+            <Email /> Email:{" "}
+            <a href="mailto:example@email.com">example@email.com</a>
+          </p>
+          <p>
+            <Phone /> Telefon: <a href="tel:+381601234567">+381 60 123 4567</a>
+          </p>
+        </div>
+        <div className="map-container">
+          <iframe
+            title="Google Map"
+            src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=43.328423,21.956343`}
+            allowFullScreen
+            loading="lazy"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
