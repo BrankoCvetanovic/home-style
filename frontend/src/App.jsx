0ba6f394180a7c0003d141e2ed4949edce3bd3ce
BahkoHatake <cbranko01@gmail.com>
2025-02-09 23:03:36 +0100

    Add Error page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import RootPage from "./pages/Root";
 import HomePage from "./pages/Home";
 import GaleryPage from "./pages/Galery";
 import ContactPage from "./pages/Contact";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootPage />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/galery", element: <GaleryPage /> },
